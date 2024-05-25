93551777f30454b260c66053bc0808658476c233
Fidelis Wangui <wmfidelis@gmail.com>
2024-05-25 03:10:27 +0300

    Add custom templates for more intuitive navigation

--- portfolio/myapp/views.py
+++ portfolio/myapp/views.py
@@ -1,57 +1,120 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import PersonalDetails, Education, WorkExperience, Skill, Project
 from .serializers import (
     PersonalDetailsSerializer, EducationSerializer, WorkExperienceSerializer,
     SkillSerializer, ProjectSerializer,
 )
+from .forms import PersonalDetailsForm, EducationForm, WorkExperienceForm, SkillForm, ProjectForm
 
-class PersonalDetailsListCreateAPIView(generics.ListCreateAPIView):
-    queryset = PersonalDetails.objects.all()
-    serializer_class = PersonalDetailsSerializer
-    permission_classes = [IsAuthenticated]
 
-class PersonalDetailsRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = PersonalDetails.objects.all()
-    serializer_class = PersonalDetailsSerializer
-    permission_classes = [IsAuthenticated]
+class PersonalDetailsListView(LoginRequiredMixin, ListView):
+    model = PersonalDetails
+    template_name = 'personal_details_list.html'
+    context_object_name = 'personal_details_list'
 
-class EducationListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Education.objects.all()
-    serializer_class = EducationSerializer
-    permission_classes = [IsAuthenticated]
+class PersonalDetailsCreateView(LoginRequiredMixin, CreateView):
+    model = PersonalDetails
+    template_name = 'personal_details_form.html'
+    form_class = PersonalDetailsForm
+    success_url = reverse_lazy('personal_details_list')
 
-class EducationRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Education.objects.all()
-    serializer_class = EducationSerializer
-    permission_classes = [IsAuthenticated]
+class PersonalDetailsUpdateView(LoginRequiredMixin, UpdateView):
+    model = PersonalDetails
+    template_name = 'personal_details_form.html'
+    form_class = PersonalDetailsForm
+    success_url = reverse_lazy('personal_details_list')
 
-class WorkExperienceListCreateAPIView(generics.ListCreateAPIView):
-    queryset = WorkExperience.objects.all()
-    serializer_class = WorkExperienceSerializer
-    permission_classes = [IsAuthenticated]
+class PersonalDetailsDeleteView(LoginRequiredMixin, DeleteView):
+    model = PersonalDetails
+    template_name = 'personal_details_confirm_delete.html'
+    success_url = reverse_lazy('personal_details_list')
 
-class WorkExperienceRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = WorkExperience.objects.all()
-    serializer_class = WorkExperienceSerializer
-    permission_classes = [IsAuthenticated]
+class EducationListView(LoginRequiredMixin, ListView):
+    model = Education
+    template_name = 'education_list.html'
+    context_object_name = 'education_list'
 
-class SkillListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Skill.objects.all()
-    serializer_class = SkillSerializer
-    permission_classes = [IsAuthenticated]
+class EducationCreateView(LoginRequiredMixin, CreateView):
+    model = Education
+    template_name = 'education_form.html'
+    form_class = EducationForm
+    success_url = reverse_lazy('education_list')
 
-class SkillRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Skill.objects.all()
-    serializer_class = SkillSerializer
-    permission_classes = [IsAuthenticated]
+class EducationUpdateView(LoginRequiredMixin, UpdateView):
+    model = Education
+    template_name = 'education_form.html'
+    form_class = EducationForm
+    success_url = reverse_lazy('education_list')
 
-class ProjectListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+class EducationDeleteView(LoginRequiredMixin, DeleteView):
+    model = Education
+    template_name = 'education_confirm_delete.html'
+    success_url = reverse_lazy('education_list')
 
-class ProjectRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+class WorkExperienceListView(LoginRequiredMixin, ListView):
+    model = WorkExperience
+    template_name = 'work_experience_list.html'
+    context_object_name = 'work_experience_list'
+
+class WorkExperienceCreateView(LoginRequiredMixin, CreateView):
+    model = WorkExperience
+    template_name = 'work_experience_form.html'
+    form_class = WorkExperienceForm
+    success_url = reverse_lazy('work_experience_list')
+
+class WorkExperienceUpdateView(LoginRequiredMixin, UpdateView):
+    model = WorkExperience
+    template_name = 'work_experience_form.html'
+    form_class = WorkExperienceForm
+    success_url = reverse_lazy('work_experience_list')
+
+class WorkExperienceDeleteView(LoginRequiredMixin, DeleteView):
+    model = WorkExperience
+    template_name = 'work_experience_confirm_delete.html'
+    success_url = reverse_lazy('work_experience_list')
+
+class SkillListView(LoginRequiredMixin, ListView):
+    model = Skill
+    template_name = 'skill_list.html'
+    context_object_name = 'skill_list'
+
+class SkillCreateView(LoginRequiredMixin, CreateView):
+    model = Skill
+    template_name = 'skill_form.html'
+    form_class = SkillForm
+    success_url = reverse_lazy('skill_list')
+
+class SkillUpdateView(LoginRequiredMixin, UpdateView):
+    model = Skill
+    template_name = 'skill_form.html'
+    form_class = SkillForm
+    success_url = reverse_lazy('skill_list')
+
+class SkillDeleteView(LoginRequiredMixin, DeleteView):
+    model = Skill
+    template_name = 'skill_confirm_delete.html'
+    success_url = reverse_lazy('skill_list')
+
+class ProjectListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = 'project_list.html'
+    context_object_name = 'project_list'
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = 'project_form.html'
+    form_class = ProjectForm
+    success_url = reverse_lazy('project_list')
+
+class ProjectUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    template_name = 'project_form.html'
+    form_class = ProjectForm
+    success_url = reverse_lazy('project_list')
+
+class ProjectDeleteView(LoginRequiredMixin, DeleteView):
+    model = Project
+    template_name = 'project_confirm_delete.html'
+    success_url = reverse_lazy('project_list')
